042a56096d8a37be987620ce61552eb4a9e507d3
myselfantara@gmail.com <myselfantara@gmail.com@34524d59-cb9f-2386-7401-b1a59f9bc9cc>
2011-03-17 17:55:40 +0000

    commit project 3...Experiences will be submitted later

---
+++ Monopoly/src/edu/ncsu/monopoly/Cell.java
@@ -0,0 +1,49 @@
+package edu.ncsu.monopoly;
+
+public abstract class Cell implements IOwnable {
+	private String name;
+	protected Player theOwner;
+	private boolean available = true;
+
+	public String getName() {
+		return name;
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.ncsu.monopoly.IOwnable#getTheOwner()
+	 */
+	@Override
+	public Player getTheOwner() {
+		return theOwner;
+	}
+	
+	public int getPrice() {
+		return 0;
+	}
+
+	public abstract boolean playAction(String msg);
+
+	void setName(String name) {
+		this.name = name;
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.ncsu.monopoly.IOwnable#setTheOwner(edu.ncsu.monopoly.Player)
+	 */
+	@Override
+	public void setTheOwner(Player owner) {
+		this.theOwner = owner;
+	}
+    
+    public String toString() {
+        return name;
+    }
+
+	public boolean isAvailable() {
+		return available;
+	}
+
+	public void setAvailable(boolean available) {
+		this.available = available;
+	}
+}
